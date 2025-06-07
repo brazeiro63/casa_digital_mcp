77dba1c9ababd26a5c3ac83dd193d53448af4edf
brazeiro63 <brazeiro63@gmail.com>
2025-06-07 10:22:26 -0300

    Designação de banco de dados no docker compose e env.py

--- test_db.py
+++ test_db.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 print(f"Usando DATABASE_URL: {DATABASE_URL}")
 
 engine = create_engine(DATABASE_URL)
